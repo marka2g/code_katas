9af88b4675c64bce2d9b304f907946f179b7c6f3
Mark Sadegi <mark.sadegi@gmail.com>
2015-11-16 20:52:55 -0800

    stay in the simple family with only testing constants

--- ruby/spec/lib/convert_arabic_to_roman_kh.rb
+++ ruby/spec/lib/convert_arabic_to_roman_kh.rb
@@ -1,9 +1,5 @@
 def convert(in_arabic)
-  # ""
-  # as we move forward getting subsequent examples to pass, we want to make sure to not have previous examples start failing
-  return "" if in_arabic.zero? #this is a guard clause - CH uses this alot as a technique to 'wall off' the previous code
-  # also note that the first failure we got was a bad value.  we expected "I" but got empty string, while this one takes us back to nil
-  # nil to constant is a wonderfully simple transformation according to TPP. we can enact that transformation to get this example passing
+  return "" if in_arabic.zero?
   "I"
 end
 
@@ -16,14 +12,22 @@ describe "Converting arabic numbers to roman numerals" do
       expect(convert(0)).to eq("")
     end
 
-    # 1st rule based case - next example is the one that adds variation to my system after setting up the structure with a degenerate case
+    # 1st rule based case
     it "converts 1 to I" do
       expect(convert(1)).to eq("I")
+    end
+
+    # up to now it was simple.  here is when we make the first decision.  2 is too complex because it implies a vector or
+    # array of 1's - i.e. 2 -> II  lets use something more simple, say, 5 -> V
+    # another factor to move on is that fact that i can create this test by a simple copy, search and replace
+      # this excentuates the fact that the test for 5 is a constant, just like 1. 
+    it "converts 5 to V" do
+      expect(convert(5)).to eq("V")
     end
   end
 end
 
 
-# I = 1, X=10, L = 50, D=500, M=1000
+# I = 1, V = 5, X=10, L = 50, D=500, M=1000
 # http://literacy.kent.edu/Minigrants/Cinci/romanchart.htm
 # based on Corey's convert kata here: http://blog.coreyhaines.com/2012/12/roman-numerals-kata-with-commentary.html
